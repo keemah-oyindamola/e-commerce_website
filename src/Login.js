9df98bab35a972deab8fb35c5cadc3b8cc5692d8
keemah-oyindamola <hikmotbolanle15@gmail.com>
2025-08-15 07:18:58 -0700

    loginnn

--- src/Login.js
+++ src/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
             password: ""
         },
         validationSchema: yup.object({
-            email: yup.string.email("Invalid email address").required("Email is required"),
+            email: yup.string().email("Invalid email address").required("Email is required"),
             password: yup.string().min(5, "Password must be at least 5 characters").required("Password is required"),
         }),
         onSubmit: async (value) => {
